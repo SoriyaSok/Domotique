bf71cc7120c4db1d87b8bc22798180d5ad5410f6
SoriyaSok <soriyasok@gmail.com>
2016-06-27 17:08:17 +0200

    jdbc 27/06/2016 17h00

--- src/com/company/domotique/application/Lanceur.java
+++ src/com/company/domotique/application/Lanceur.java
@@ -1,4 +1,11 @@
 package com.company.domotique.application;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.company.domotique.appareils.AppareilElectrique;
 import com.company.domotique.appareils.AppareilThermostate;
 import com.company.domotique.exceptions.CompteurADisjoncteException;
@@ -103,6 +110,72 @@ public class Lanceur {
 				System.out.println("le compteur : " + e.getCompteurEnCause().getMarque() 
 						+ " a disjoncté pour la puissance " + e.getPuissanceDemandee());
 			}
+			
+			//-------------- JDBC --------------------------//
+			Connection connexion=null;
+			try{
+				// 1) charger dynamiquement la classe du pilote
+				Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+				// 2) fournir le nom de la source de donnees
+				String sPseudoURL = "jdbc:odbc:DSNDomotique";
+				// 3)Etablir la connexion  
+				connexion=DriverManager.getConnection(sPseudoURL);
+			} catch (ClassNotFoundException e){
+				System.out.println("Pilote non trouv�");
+				System.exit(1);
+			} catch(SQLException e){
+				System.out.println("Erreur :"+e+" sur ");
+				System.exit(1);
+			}//catch
+			
+			try{
+				Statement laRequete = connexion.createStatement();
+				ResultSet leResultat = laRequete.executeQuery("select * from APPAREIL_ELECTRIQUE_TBL");
+				
+				// on peut fournir un tableau String[] lesTypes={ "TABLE" }; 
+				// en fait on veut tout, d'o� le null ci-dessous
+				
+				//------ Boucle sur les tables ------
+				while (leResultat.next()){
+					int columns = leResultat.getMetaData().getColumnCount();
+					String ligne = "";
+					for (int i = 1; i <= columns; i++) {
+						ligne += leResultat.getString(i) + " | ";
+					}
+					System.out.println(ligne);
+					
+					
+//					System.out.println("R�pertoire - Schema - Nom - Type de Table -");
+//					System.out.print(rsTables.getString("TABLE_CAT")+" - ");
+//					System.out.print(rsTables.getString("TABLE_SCHEM")+" - ");
+//					String sTable=rsTables.getString("TABLE_NAME");
+//					System.out.print(sTable+" - ");
+//					System.out.println(rsTables.getString("TABLE_TYPE"));
+//					System.out.println("--------------------------------------------");
+//					//--------- Boucle sur les champs de la table
+//					ResultSet rsColonnes=dbmd.getColumns(null,null,sTable,null);
+//					System.out.println("Nom - Type - Sous Type - Taille de colonne");
+//						while (rsColonnes.next()){
+//							System.out.print(rsColonnes.getString("COLUMN_NAME")+" - ");
+//							System.out.print(rsColonnes.getShort("DATA_TYPE")+" - ");
+//							String sType=rsColonnes.getString("TYPE_NAME");
+//							System.out.print(sType+" - ");
+//							if ( sType.equals("CHAR")){
+//								System.out.println(rsColonnes.getInt("CHAR_OCTET_LENGTH")+" - ");
+//							}
+//							else {
+//								System.out.println(rsColonnes.getInt("COLUMN_SIZE")+" - ");
+//							}
+//						}//while les colonnes
+//						rsColonnes.close();
+//						System.out.println("----------------------------------------");
+				}//while les tables
+				leResultat.close();
+			}
+			catch(SQLException e){
+							System.out.println("Erreur :"+e+" sur ");
+							System.exit(1);
+			}//catch
 			
 			CadreGeneral monInterface = new CadreGeneral(unOrdinateur1, unMicroOnde, unCompteurEDF);	
 	}
